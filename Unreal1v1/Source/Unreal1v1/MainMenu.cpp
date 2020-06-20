b1cbdd359301fd15e1fb72b0b02e43aaf57f88a9
mbpmm <marianomcr92@gmail.com>
2020-06-20 19:09:27 -0300

    Added Main Menu

---
+++ Unreal1v1/Source/Unreal1v1/MainMenu.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenu.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/Button.h"
+#include "Components/VerticalBox.h"
+#include "Blueprint/WidgetTree.h"
+
+void UMainMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+}
+
+void UMainMenu::OnButtonClicked()
+{
+
+}
---
+++ Unreal1v1/Source/Unreal1v1/MainMenu.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL1V1_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(Transient)
+		TArray<class UButton*> Buttons;
+
+	UPROPERTY(meta = (BindWidget))
+		class UVerticalBox* VBoxButtons;
+
+	virtual void NativeOnInitialized() override;
+
+	void OnButtonClicked();
+	
+};
